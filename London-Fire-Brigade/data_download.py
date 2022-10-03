41dc5039ab11429f7dcb91c1f6e5df6513c7d091
Ndaruga <ndaruga.francis@gmail.com>
2022-10-03 15:18:11 +0300

    import files

--- London-Fire-Brigade/data_download.py
+++ London-Fire-Brigade/data_download.py
@@ -12,7 +12,8 @@ data_dir = "LFB-data"
 URL = "https://data.london.gov.uk/download/london-fire-brigade-incident-records/73728cf4-b70e-48e2-9b97-4e4341a2110d/LFB%20Incident%20data%20-%20Datastore%20-%20with%20notional%20cost%20and%20UPRN%20from%20January%202009%20%282%29.zip"
 
 os.chdir(data_dir)
-if (os.path.isdir("archive.zip")) == False:
+if (os.path.isdir("archive")) == False:
+    print('Starting download ...')
     data = requests.get(URL, allow_redirects=True)
     open("archive.zip", "wb").write(data.content)
     print("Download Complete\n\nExtracting ...")
@@ -21,5 +22,9 @@ if (os.path.isdir("archive.zip")) == False:
 
     patoolib.extract_archive('archive.zip')
     print("\nExtract complete")
+    
+
 else:
     print("\nFiles Already Extracted")
+
+
