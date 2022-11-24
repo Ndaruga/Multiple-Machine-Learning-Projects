a46e3a5ef81a3c6c34a7bd6d354a97bd3023c3a3
Ndaruga <ndaruga.francis@gmail.com>
2022-11-24 08:52:51 +0300

    data download

--- London-Fire-Brigade/data_download.py
+++ London-Fire-Brigade/data_download.py
@@ -1,6 +1,6 @@
 # This file will download the latest version of the London Fire Brigade data
 
-# we will import it into the main working file
+# we will import it into the main working file as a library
 
 import requests
 #pip install patool
@@ -31,24 +31,27 @@ def download_data(URL, filename):
 
 data_dir = "LFB-data"
 
-URL = "https://data.london.gov.uk/download/london-fire-brigade-incident-records/73728cf4-b70e-48e2-9b97-4e4341a2110d/LFB%20Incident%20data%20-%20Datastore%20-%20with%20notional%20cost%20and%20UPRN%20from%20January%202009%20%282%29.zip"
+URL = "https://data.london.gov.uk/download/london-fire-brigade-incident-records/f5066d66-c7a3-415f-9629-026fbda61822/LFB%20Incident%20data%20Last%203%20years%20-%20with%20num%20calls.xlsx"
+#URL = "https://data.london.gov.uk/download/london-fire-brigade-incident-records/73728cf4-b70e-48e2-9b97-4e4341a2110d/LFB%20Incident%20data%20-%20Datastore%20-%20with%20notional%20cost%20and%20UPRN%20from%20January%202009%20%282%29.zip"
 
 
-
+if os.path.exists(data_dir) == False:
+    os.mkdir(data_dir)
+else:
+    pass
 
 os.chdir(data_dir)
-if (os.path.exists("archive.zip")) == True:
 
 # Remove files in the directory
-    my_files = glob.glob(data_dir)
-    for files in my_files:
-        os.remove(files)
+my_files = glob.glob(data_dir)
+for files in my_files:
+    os.remove(files)
 
 # Download files
-    print('Starting download ...')
-    download_data(URL, "archive.zip")
-    print("\n")
-
+print('Starting download ...')
+download_data(URL, "lfb-incidents-for-last-3-years.xlsx")
+print("\n")
+'''
 else:
     print('Starting download ...')
     download_data(URL, "archive.zip")    
@@ -56,4 +59,4 @@ else:
     patoolib.extract_archive('archive.zip')
     print("\nExtract complete")
 
-
+'''
